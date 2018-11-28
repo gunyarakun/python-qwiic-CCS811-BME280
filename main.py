2501406b1f239a26055cd9d9229dcad9fe2f545a
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2018-11-28 10:56:58 +0000

    Refactor

--- main.py
+++ main.py
@@ -10,36 +10,8 @@ import slack_notifier
 from CCS811 import CCS811
 
 class AirConditionMonitor:
-    CO2_PPM_THRESHOLD_1 = 1000
-    CO2_PPM_THRESHOLD_2 = 2000
-
     CO2_LOWER_LIMIT  =  400
     CO2_HIGHER_LIMIT = 8192
-
-    CO2_STATUS_CONDITIONING = 'CONDITIONING'
-    CO2_STATUS_LOW          = 'LOW'
-    CO2_STATUS_HIGH         = 'HIGH'
-    CO2_STATUS_TOO_HIGH     = 'TOO HIGH'
-    CO2_STATUS_ERROR        = 'ERROR'
-
-    CO2_LOW_MESSAGE = u'''
-室内の二酸化炭素濃度は{0:,}ppmに落ち着きました。
-換気してくれた方ありがとう。
-'''
-    CO2_HIGH_MESSAGE = u'''
-室内の二酸化炭素濃度は{0:,}ppmで、{1:,}ppmを超えています。
-眠気・倦怠感・頭痛・耳鳴り・息苦しさ等の原因となります。
-換気しましょう！
-'''
-    CO2_TOO_HIGH_MESSAGE = u'''
-室内の二酸化炭素濃度は{0:,}ppmで、{1:,}ppmを超えています。
-換気！換気！さっさと換気！
-'''
-
-    SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/XXXXXXXXX/XXXXXXXXX/XXXXXXXXXXXXXXXXXXXXXXXX'
-    SLACK_USERNAME = u'二酸化炭素濃度測るくん'
-    SLACK_CHANNEL_ERROR = '#error'
-    SLACK_CHANNEL_NOTIFY = '#notify'
 
     LOG_FILE = '{script_dir}/logs/air_condition_monitor.log'.format(
         script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -47,8 +19,7 @@ class AirConditionMonitor:
 
     def __init__(self):
         self._ccs811 = CCS811()
-        self.slack_notifier = slack_notifier.SlackNotifier(self.SLACK_WEBHOOK_URL)
-        self.co2_status = self.CO2_STATUS_LOW
+        self.slack_notifier = slack_notifier.SlackNotifier()
         self.init_logger()
 
     def init_logger(self):
@@ -58,40 +29,6 @@ class AirConditionMonitor:
         file_handler.setFormatter(formatter)
         self._logger.addHandler(file_handler)
         self._logger.setLevel(DEBUG)
-
-    def notify_to_slack(self, co2, co2_status):
-        if co2_status == self.CO2_STATUS_ERROR:
-            fallback = "Something Went Wrong..."
-            text = fallback
-            color = 'danger'
-            channel = self.SLACK_CHANNEL_ERROR
-            title = u'二酸化炭素濃度botでエラーです'
-        else:
-            channel = self.SLACK_CHANNEL_NOTIFY
-            title = u'二酸化炭素濃度botからのお知らせ'
-
-            if co2_status == self.CO2_STATUS_LOW:
-                color = 'good'
-                fallback = self.CO2_LOW_MESSAGE.format(co2, self.CO2_PPM_THRESHOLD_1)
-            elif co2_status == self.CO2_STATUS_HIGH:
-                color = 'warning'
-                fallback = self.CO2_HIGH_MESSAGE.format(co2, self.CO2_PPM_THRESHOLD_1)
-            elif co2_status == self.CO2_STATUS_TOO_HIGH:
-                color = 'danger'
-                fallback = self.CO2_TOO_HIGH_MESSAGE.format(co2, self.CO2_PPM_THRESHOLD_2)
-            else:
-                return
-
-            text = fallback
-
-        res = self.slack_notifier.notify(
-            fallback, title, text, self.SLACK_USERNAME, channel, color
-        )
-        self._logger.debug(
-            'Notified to Slack. STATUS_CODE - {status_code}, MSG - {msg}'.format(
-                status_code = res['status_code'], msg = res['text']
-            )
-        )
 
     def status(self, co2):
         if co2 < self.CO2_LOWER_LIMIT or co2 > self.CO2_HIGHER_LIMIT:
@@ -115,24 +52,19 @@ class AirConditionMonitor:
             try:
                 if not self._ccs811.readData():
                     co2 = self._ccs811.geteCO2()
-                    co2_status = self.status(co2)
-                    if co2_status == self.CO2_STATUS_CONDITIONING:
+
+                    if co2 < self.CO2_LOWER_LIMIT or co2 > self.CO2_HIGHER_LIMIT:
                         self._logger.debug("Under Conditioning...")
                         sleep(2)
                         continue
 
-                    if co2_status != self.co2_status:
-                        self.notify_to_slack(co2, co2_status)
-                        self.co2_status = co2_status
+                    self.slack_notifier.update_co2(co2)
 
                     self._logger.info("CO2: {0}ppm, TVOC: {1}".format(co2, self._ccs811.getTVOC()))
                 else:
                     self._logger.error('ERROR!')
-                    while True:
-                        pass
             except:
                 self._logger.error(sys.exc_info())
-                # self.notify_to_slack(-1, self.CO2_STATUS_ERROR)
 
             sleep(2)
 
--- slack_notifier.py
+++ slack_notifier.py
@@ -1,11 +1,94 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+import os
 import json
 import requests
+from logging import getLogger, DEBUG, FileHandler, Formatter
 
 class SlackNotifier:
-    def __init__(self, webhook_url):
-        self.webhook_url = webhook_url
+    CO2_PPM_THRESHOLD_1 = 1000
+    CO2_PPM_THRESHOLD_2 = 2000
+
+    CO2_STATUS_LOW          = 'LOW'
+    CO2_STATUS_HIGH         = 'HIGH'
+    CO2_STATUS_TOO_HIGH     = 'TOO HIGH'
+
+    CO2_LOW_MESSAGE = u'''
+室内の二酸化炭素濃度は{0:,}ppmに落ち着きました。
+換気してくれた方ありがとう。
+'''
+    CO2_HIGH_MESSAGE = u'''
+室内の二酸化炭素濃度は{0:,}ppmで、{1:,}ppmを超えています。
+眠気・倦怠感・頭痛・耳鳴り・息苦しさ等の原因となります。
+換気しましょう！
+'''
+    CO2_TOO_HIGH_MESSAGE = u'''
+室内の二酸化炭素濃度は{0:,}ppmで、{1:,}ppmを超えています。
+換気！換気！さっさと換気！
+'''
+
+    SLACK_USERNAME = u'二酸化炭素濃度測るくん'
+    SLACK_WEBHOOK_URL    = 'https://hooks.slack.com/services/XXXXXXXXX/XXXXXXXXX/XXXXXXXXXXXXXXXXXXXXXXXX'
+    SLACK_CHANNEL_NOTIFY = '#channel'
+
+    LOG_FILE = '{script_dir}/logs/slack.log'.format(
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+    )
+
+    def __init__(self):
+        self.init_logger()
+        self.co2_status = self.CO2_STATUS_LOW
+
+    def init_logger(self):
+        self._logger = getLogger(self.__class__.__name__)
+        file_handler = FileHandler(self.LOG_FILE)
+        formatter = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(formatter)
+        self._logger.addHandler(file_handler)
+        self._logger.setLevel(DEBUG)
+
+    def convert_co2_status(self, co2):
+        if co2 < self.CO2_PPM_THRESHOLD_1:
+            return self.CO2_STATUS_LOW
+        elif co2 < self.CO2_PPM_THRESHOLD_2:
+            return self.CO2_STATUS_HIGH
+        else:
+            return self.CO2_STATUS_TOO_HIGH
+
+    def update_co2(self, co2):
+        co2_status = self.convert_co2_status(co2)
+        if co2_status != self.co2_status:
+            self.notify_to_slack(co2, co2_status)
+            self.co2_status = co2_status
+
+    def notify_to_slack(self, co2, co2_status):
+        channel = self.SLACK_CHANNEL_NOTIFY
+        title = u'二酸化炭素濃度botからのお知らせ'
+
+        if co2_status == self.CO2_STATUS_LOW:
+            color = 'good'
+            fallback = self.CO2_LOW_MESSAGE.format(co2, self.CO2_PPM_THRESHOLD_1)
+        elif co2_status == self.CO2_STATUS_HIGH:
+            color = 'warning'
+            fallback = self.CO2_HIGH_MESSAGE.format(co2, self.CO2_PPM_THRESHOLD_1)
+        elif co2_status == self.CO2_STATUS_TOO_HIGH:
+            color = 'danger'
+            fallback = self.CO2_TOO_HIGH_MESSAGE.format(co2, self.CO2_PPM_THRESHOLD_2)
+        else:
+            return
+
+        text = fallback
+
+        res = self.notify(
+            fallback, title, text, self.SLACK_USERNAME, channel, color
+        )
+        self._logger.debug(
+            'Notified to Slack. STATUS_CODE - {status_code}, MSG - {msg}'.format(
+                status_code = res['status_code'], msg = res['text']
+            )
+        )
+
 
     def notify(self, fallback, title, text, username, channel, color):
         headers = {
@@ -25,7 +108,7 @@ class SlackNotifier:
             ]
         }
 
-        response = requests.post(self.webhook_url, json = payload, headers = headers)
+        response = requests.post(self.SLACK_WEBHOOK_URL, json = payload, headers = headers)
 
         return {
             'status_code': response.status_code,
